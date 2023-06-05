daf487500b6d0cc52d07dadd79527cfe259acd2f
Carlos Perez <per.ckarlos@gmail.com>
2023-06-04 23:32:49 -0500

    adding render and api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common';
+import { Product } from './models/product.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'midudev-app';
+  http = inject(HttpClient);
+  products: Product[] = [];
+
+  changeTitle() {
+    this.title = 'changed';
+  }
+
+  ngOnInit() {
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data) => {
+        this.products = data;
+      });
+  }
 }
